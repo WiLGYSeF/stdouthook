5d2b76525633ce0a8dbb9f5c7eb1265544b7a21d
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2023-05-08 19:23:05 -0400

    add byte format

---
+++ Tests/Wilgysef.StdoutHook.Tests/FormatterTests/ByteFormatBuilderTest.cs
@@ -0,0 +1,23 @@
+﻿using Wilgysef.StdoutHook.Formatters.FormatBuilders;
+using Wilgysef.StdoutHook.Profiles;
+
+namespace Wilgysef.StdoutHook.Tests.FormatterTests;
+
+public class ByteFormatBuilderTest : RuleTestBase
+{
+    [Fact]
+    public void Byte()
+    {
+        var formatter = GetFormatter(new ByteFormatBuilder());
+
+        formatter.Format("test%x41", new DataState(new ProfileState())).ShouldBe("testA");
+    }
+
+    [Fact]
+    public void Invalid()
+    {
+        var formatter = GetFormatter(new ByteFormatBuilder());
+
+        formatter.Format("test%xgh", new DataState(new ProfileState())).ShouldBe("test");
+    }
+}
--- Wilgysef.StdoutHook/Formatters/FormatFunctionBuilder.cs
+++ Wilgysef.StdoutHook/Formatters/FormatFunctionBuilder.cs
@@ -9,6 +9,7 @@ namespace Wilgysef.StdoutHook.Formatters
     {
         public static FormatBuilder[] FormatBuilders = new FormatBuilder[]
         {
+            new ByteFormatBuilder(),
             new ColorFormatBuilder(),
             new FieldFormatBuilder(),
             new RegexGroupFormatBuilder(),
