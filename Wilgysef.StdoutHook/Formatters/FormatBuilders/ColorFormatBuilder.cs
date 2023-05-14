dfcc16275d7c81c894aa9ddcfb321a5784db06d1
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2023-05-14 13:37:56 -0400

    add custom colors

--- Wilgysef.StdoutHook/Formatters/FormatBuilders/ColorFormatBuilder.cs
+++ Wilgysef.StdoutHook/Formatters/FormatBuilders/ColorFormatBuilder.cs
@@ -15,60 +15,205 @@ namespace Wilgysef.StdoutHook.Formatters.FormatBuilders
 
         static ColorFormatBuilder()
         {
-            AddColor(new Color("black", "k", 30));
-            AddColor(new Color("red", "r", 31));
-            AddColor(new Color("green", "g", 32));
-            AddColor(new Color("yellow", "y", 33));
-            AddColor(new Color("blue", "b", 34));
-            AddColor(new Color("magenta", "m", 35));
-            AddColor(new Color("cyan", "c", 36));
-            AddColor(new Color("gray", 37));
-            AddColor(new Color("grey", "e", 37));
-            AddColor(new Color("default", 39));
+            #region Builtin Colors
 
-            AddColor(new Color("darkgray", 90));
-            AddColor(new Color("darkgrey", "de", 90));
-            AddColor(new Color("lightblack", "lk", 90));
-            AddColor(new Color("lightred", "lr", 91));
-            AddColor(new Color("lightgreen", "lg", 92));
-            AddColor(new Color("lightyellow", "ly", 93));
-            AddColor(new Color("lightblue", "lb", 94));
-            AddColor(new Color("lightmagenta", "lm", 95));
-            AddColor(new Color("lightcyan", "lc", 96));
-            AddColor(new Color("lightgray", 97));
-            AddColor(new Color("lightgrey", "le", 97));
-            AddColor(new Color("white", "w", 97));
+            AddColor("black", "k", 30, 40);
+            AddColor("red", "r", 31, 41);
+            AddColor("green", "g", 32, 42);
+            AddColor("yellow", "y", 33, 43);
+            AddColor("blue", "b", 34, 44);
+            AddColor("magenta", "m", 35, 45);
+            AddColor("cyan", "c", 36, 46);
+            AddColor("gray", null, 37, 47);
+            AddColor("grey", "e", 37, 47);
+            AddColor("default", null, 39, 49);
 
-            AddColor(new Style("z", 0, 0));
-            AddColor(new Style("reset", "res", 0, 0));
-            AddColor(new Style("normal", "nor", 0, 0));
-            AddColor(new Style("bold", "bol", 1));
-            AddColor(new Style("bright", "bri", 1));
-            AddColor(new Style("dim", 2));
-            AddColor(new Style("italic", "ita", 3));
-            AddColor(new Style("underline", "ul", 4));
-            AddColor(new Style("underlined", "und", 4));
-            AddColor(new Style("blink", "bli", 5));
-            AddColor(new Style("invert", "inv", 7));
-            AddColor(new Style("reverse", "rev", 7));
-            AddColor(new Style("hidden", "hid", 8));
-            AddColor(new Style("conceal", "con", 8));
-            AddColor(new Style("strike", "str", 9));
-            AddColor(new Style("strikethrough", 9));
-            AddColor(new Style("crossed", "cro", 9));
-            AddColor(new Style("crossedout", 9));
+            AddColor("darkgray", null, 90, 100);
+            AddColor("darkgrey", "de", 90, 100);
+            AddColor("lightblack", "lk", 90, 100);
+            AddColor("lightred", "lr", 91, 101);
+            AddColor("lightgreen", "lg", 92, 102);
+            AddColor("lightyellow", "ly", 93, 103);
+            AddColor("lightblue", "lb", 94, 104);
+            AddColor("lightmagenta", "lm", 95, 105);
+            AddColor("lightcyan", "lc", 96, 106);
+            AddColor("lightgray", null, 97, 107);
+            AddColor("lightgrey", "le", 97, 107);
+            AddColor("white", "w", 97, 107);
 
-            AddColor(new Style("overline", "ol", 53, 55));
-            AddColor(new Style("overlined", "ove", 53, 55));
+            AddColor("z", null, 0, 0);
+            AddColor("reset", "res", 0, 0);
+            AddColor("normal", "nor", 0, 0);
+            AddColor("bold", "bol", 1, 21);
+            AddColor("bright", "bri", 1, 21);
+            AddColor("dim", null, 2, 22);
+            AddColor("italic", "ita", 3, 23);
+            AddColor("underline", "ul", 4, 24);
+            AddColor("underlined", "und", 4, 24);
+            AddColor("blink", "bli", 5, 25);
+            AddColor("invert", "inv", 7, 27);
+            AddColor("reverse", "rev", 7, 27);
+            AddColor("hidden", "hid", 8, 28);
+            AddColor("conceal", "con", 8, 28);
+            AddColor("strike", "str", 9, 29);
+            AddColor("strikethrough", null, 9, 29);
+            AddColor("crossed", "cro", 9, 29);
+            AddColor("crossedout", null, 9, 29);
 
-            static void AddColor(Color color)
+            AddColor("overline", "ol", 53, 55);
+            AddColor("overlined", "ove", 53, 55);
+
+            #endregion
+
+            #region Custom Colors
+
+            AddCustomColor("aliceblue", 0xf0f8ff);
+            AddCustomColor("antiquewhite", 0xfaebd7);
+            AddCustomColor("aqua", 0x00ffff);
+            AddCustomColor("aquamarine", 0x7fffd4);
+            AddCustomColor("azure", 0xf0ffff);
+            AddCustomColor("beige", 0xf5f5dc);
+            AddCustomColor("bisque", 0xffe4c4);
+            AddCustomColor("blanchedalmond", 0xffebcd);
+            AddCustomColor("blueviolet", 0x8a2be2);
+            AddCustomColor("brown", 0xa52a2a);
+            AddCustomColor("burlywood", 0xdeb887);
+            AddCustomColor("cadetblue", 0x5f9ea0);
+            AddCustomColor("chartreuse", 0x7fff00);
+            AddCustomColor("chocolate", 0xd2691e);
+            AddCustomColor("coral", 0xff7f50);
+            AddCustomColor("cornflowerblue", 0x6495ed);
+            AddCustomColor("cornsilk", 0xfff8dc);
+            AddCustomColor("crimson", 0xdc143c);
+            AddCustomColor("darkblue", 0x00008b);
+            AddCustomColor("darkcyan", 0x008b8b);
+            AddCustomColor("darkgoldenrod", 0xb8860b);
+            AddCustomColor("darkgreen", 0x006400);
+            AddCustomColor("darkkhaki", 0xbdb76b);
+            AddCustomColor("darkmagenta", 0x8b008b);
+            AddCustomColor("darkolivegreen", 0x556b2f);
+            AddCustomColor("darkorange", 0xff8c00);
+            AddCustomColor("darkorchid", 0x9932cc);
+            AddCustomColor("darkred", 0x8b0000);
+            AddCustomColor("darksalmon", 0xe9967a);
+            AddCustomColor("darkseagreen", 0x8fbc8f);
+            AddCustomColor("darkslateblue", 0x483d8b);
+            AddCustomColor("darkslategray", 0x2f4f4f);
+            AddCustomColor("darkslategrey", 0x2f4f4f);
+            AddCustomColor("darkturquoise", 0x00ced1);
+            AddCustomColor("darkviolet", 0x9400d3);
+            AddCustomColor("deeppink", 0xff1493);
+            AddCustomColor("deepskyblue", 0x00bfff);
+            AddCustomColor("dimgray", 0x696969);
+            AddCustomColor("dimgrey", 0x696969);
+            AddCustomColor("dodgerblue", 0x1e90ff);
+            AddCustomColor("firebrick", 0xb22222);
+            AddCustomColor("floralwhite", 0xfffaf0);
+            AddCustomColor("forestgreen", 0x228b22);
+            AddCustomColor("fuchsia", 0xff00ff);
+            AddCustomColor("gainsboro", 0xdcdcdc);
+            AddCustomColor("ghostwhite", 0xf8f8ff);
+            AddCustomColor("gold", 0xffd700);
+            AddCustomColor("goldenrod", 0xdaa520);
+            AddCustomColor("greenyellow", 0xadff2f);
+            AddCustomColor("honeydew", 0xf0fff0);
+            AddCustomColor("hotpink", 0xff69b4);
+            AddCustomColor("indianred", 0xcd5c5c);
+            AddCustomColor("indigo", 0x4b0082);
+            AddCustomColor("ivory", 0xfffff0);
+            AddCustomColor("khaki", 0xf0e68c);
+            AddCustomColor("lavender", 0xe6e6fa);
+            AddCustomColor("lavenderblush", 0xfff0f5);
+            AddCustomColor("lawngreen", 0x7cfc00);
+            AddCustomColor("lemonchiffon", 0xfffacd);
+            AddCustomColor("lightcoral", 0xf08080);
+            AddCustomColor("lightgoldenrodyellow", 0xfafad2);
+            AddCustomColor("lightpink", 0xffb6c1);
+            AddCustomColor("lightsalmon", 0xffa07a);
+            AddCustomColor("lightseagreen", 0x20b2aa);
+            AddCustomColor("lightskyblue", 0x87cefa);
+            AddCustomColor("lightslategray", 0x778899);
+            AddCustomColor("lightslategrey", 0x778899);
+            AddCustomColor("lightsteelblue", 0xb0c4de);
+            AddCustomColor("lime", 0x00ff00);
+            AddCustomColor("limegreen", 0x32cd32);
+            AddCustomColor("linen", 0xfaf0e6);
+            AddCustomColor("maroon", 0x800000);
+            AddCustomColor("mediumaquamarine", 0x66cdaa);
+            AddCustomColor("mediumblue", 0x0000cd);
+            AddCustomColor("mediumorchid", 0xba55d3);
+            AddCustomColor("mediumpurple", 0x9370db);
+            AddCustomColor("mediumseagreen", 0x3cb371);
+            AddCustomColor("mediumslateblue", 0x7b68ee);
+            AddCustomColor("mediumspringgreen", 0x00fa9a);
+            AddCustomColor("mediumturquoise", 0x48d1cc);
+            AddCustomColor("mediumvioletred", 0xc71585);
+            AddCustomColor("midnightblue", 0x191970);
+            AddCustomColor("mintcream", 0xf5fffa);
+            AddCustomColor("mistyrose", 0xffe4e1);
+            AddCustomColor("moccasin", 0xffe4b5);
+            AddCustomColor("navajowhite", 0xffdead);
+            AddCustomColor("navy", 0x000080);
+            AddCustomColor("oldlace", 0xfdf5e6);
+            AddCustomColor("olive", 0x808000);
+            AddCustomColor("olivedrab", 0x6b8e23);
+            AddCustomColor("orange", 0xffa500);
+            AddCustomColor("orangered", 0xff4500);
+            AddCustomColor("orchid", 0xda70d6);
+            AddCustomColor("palegoldenrod", 0xeee8aa);
+            AddCustomColor("palegreen", 0x98fb98);
+            AddCustomColor("paleturquoise", 0xafeeee);
+            AddCustomColor("palevioletred", 0xdb7093);
+            AddCustomColor("papayawhip", 0xffefd5);
+            AddCustomColor("peachpuff", 0xffdab9);
+            AddCustomColor("peru", 0xcd853f);
+            AddCustomColor("pink", 0xffc0cb);
+            AddCustomColor("plum", 0xdda0dd);
+            AddCustomColor("powderblue", 0xb0e0e6);
+            AddCustomColor("purple", 0x800080);
+            AddCustomColor("rebeccapurple", 0x663399);
+            AddCustomColor("rosybrown", 0xbc8f8f);
+            AddCustomColor("royalblue", 0x4169e1);
+            AddCustomColor("saddlebrown", 0x8b4513);
+            AddCustomColor("salmon", 0xfa8072);
+            AddCustomColor("sandybrown", 0xf4a460);
+            AddCustomColor("seagreen", 0x2e8b57);
+            AddCustomColor("seashell", 0xfff5ee);
+            AddCustomColor("sienna", 0xa0522d);
+            AddCustomColor("silver", 0xc0c0c0);
+            AddCustomColor("skyblue", 0x87ceeb);
+            AddCustomColor("slateblue", 0x6a5acd);
+            AddCustomColor("slategray", 0x708090);
+            AddCustomColor("slategrey", 0x708090);
+            AddCustomColor("snow", 0xfffafa);
+            AddCustomColor("springgreen", 0x00ff7f);
+            AddCustomColor("steelblue", 0x4682b4);
+            AddCustomColor("tan", 0xd2b48c);
+            AddCustomColor("teal", 0x008080);
+            AddCustomColor("thistle", 0xd8bfd8);
+            AddCustomColor("tomato", 0xff6347);
+            AddCustomColor("turquoise", 0x40e0d0);
+            AddCustomColor("violet", 0xee82ee);
+            AddCustomColor("wheat", 0xf5deb3);
+            AddCustomColor("whitesmoke", 0xf5f5f5);
+            AddCustomColor("yellowgreen", 0x9acd32);
+
+            #endregion
+
+            static void AddColor(string name, string? shortName, int value, int toggleValue)
             {
-                Colors.Add(color.Name, color);
+                var color = new Color(value, toggleValue, true);
+                Colors.Add(name, color);
 
-                if (color.ShortName != null)
+                if (shortName != null)
                 {
-                    Colors.Add(color.ShortName, color);
+                    Colors.Add(shortName, color);
                 }
+            }
+
+            static void AddCustomColor(string name, int value)
+            {
+                Colors.Add(name, new Color(value, value, false));
             }
         }
 
@@ -84,7 +229,8 @@ namespace Wilgysef.StdoutHook.Formatters.FormatBuilders
 
             if (state.Contents.StartsWith("raw"))
             {
-                return _ => $"\x1b[{state.Contents[3..]}m";
+                var rawColor = state.Contents[3..];
+                return _ => $"\x1b[{rawColor}m";
             }
 
             var colors = new List<string>(state.Contents.Split(Separator));
@@ -99,7 +245,9 @@ namespace Wilgysef.StdoutHook.Formatters.FormatBuilders
                     if (aliasColors.Length > 1)
                     {
                         colors[i] = aliasColors[0].Trim();
-                        colors.InsertRange(i + 1, aliasColors.Skip(1).Select(c => c.Trim()));
+                        colors.InsertRange(
+                            i + 1,
+                            aliasColors.Skip(1).Select(c => c.Trim()));
                     }
                     else
                     {
@@ -132,7 +280,14 @@ namespace Wilgysef.StdoutHook.Formatters.FormatBuilders
 
                 if (Colors.TryGetValue(colorStr, out var color))
                 {
-                    colorResults.Add(color.GetValue(toggle));
+                    if (color.IsSimple)
+                    {
+                        colorResults.Add(toggle ? color.ToggleValue : color.Value);
+                    }
+                    else
+                    {
+                        AddColor24Bit(colorResults, color.Value, toggle);
+                    }
                 }
                 else if (int.TryParse(colorStr, out var colorInt) && colorInt >= 0 && colorInt <= 255)
                 {
@@ -142,11 +297,7 @@ namespace Wilgysef.StdoutHook.Formatters.FormatBuilders
                 }
                 else if (TryParseHexToInt(colorStr, out var colorHex))
                 {
-                    colorResults.Add(toggle ? 48 : 38);
-                    colorResults.Add(2);
-                    colorResults.Add((colorHex >> 16) & 0xFF);
-                    colorResults.Add((colorHex >> 8) & 0xFF);
-                    colorResults.Add(colorHex & 0xFF);
+                    AddColor24Bit(colorResults, colorHex, toggle);
                 }
             }
 
@@ -158,6 +309,15 @@ namespace Wilgysef.StdoutHook.Formatters.FormatBuilders
             // only close over the result
             var result = $"\x1b[{string.Join(';', colorResults)}m";
             return _ => result;
+
+            static void AddColor24Bit(List<int> colors, int value, bool toggle)
+            {
+                colors.Add(toggle ? 48 : 38);
+                colors.Add(2);
+                colors.Add((value >> 16) & 0xFF);
+                colors.Add((value >> 8) & 0xFF);
+                colors.Add(value & 0xFF);
+            }
         }
 
         private static bool TryParseHexToInt(string hex, out int value)
@@ -192,43 +352,18 @@ namespace Wilgysef.StdoutHook.Formatters.FormatBuilders
 
         private class Color
         {
-            public string Name => _name;
+            public int Value { get; }
 
-            public string? ShortName => _shortName;
+            public int ToggleValue { get; }
 
-            protected readonly int _value;
-            private readonly string _name;
-            private readonly string? _shortName;
+            public bool IsSimple { get; }
 
-            public Color(string name, int value) : this(name, null, value) { }
-
-            public Color(string name, string? shortName, int value)
+            public Color(int value, int toggleValue, bool isSimple)
             {
-                _name = name;
-                _shortName = shortName;
-                _value = value;
+                Value = value;
+                ToggleValue = toggleValue;
+                IsSimple = isSimple;
             }
-
-            public virtual int GetValue(bool toggle = false) => toggle ? _value + 10 : _value;
-        }
-
-        private class Style : Color
-        {
-            private readonly int? _toggleValue;
-
-            public Style(string name, int value, int? toggleValue = null) : base(name, value)
-            {
-                _toggleValue = toggleValue;
-            }
-
-            public Style(string name, string? shortName, int value, int? toggleValue = null) : base(name, shortName, value)
-            {
-                _toggleValue = toggleValue;
-            }
-
-            public override int GetValue(bool toggle = false) => toggle
-                ? _toggleValue ?? _value + 20
-                : _value;
         }
     }
 }
